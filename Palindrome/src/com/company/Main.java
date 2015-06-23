f84366b82d72c4e193a5b8ddfe702175eb70c514
ohassan1 <nife.hassan@gmail.com>
2015-06-23 16:27:00 -0500

    Updating the files

--- Palindrome/src/com/company/Main.java
+++ Palindrome/src/com/company/Main.java
@@ -3,25 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String originalString =  "racecar is a palindrome";
+        String originalString =  "This is a test to tell if the code can handle a longer sentence but really i am just bored and I don't think there is a single palindrome in this so... bob BOB Bob pop racecar";
 
-        // Split the string into words with the for loop
-        for (String splitString : originalString.split(" ")) {
+
+        System.out.println("Here are your palindromes from you sentence: ");
+
+        // Split the string into words with the for loop, toLower is to get rid of capitalization errors
+        for (String splitString : originalString.toLowerCase().split(" ")) {
+
             // StringBuilders to allow manipulation
-            StringBuilder sb = new StringBuilder(splitString);
+            StringBuilder words = new StringBuilder(splitString);
 
-            StringBuilder sb2 = new StringBuilder(sb);
+            StringBuilder reversedStringSplits = new StringBuilder(words);
 
-            // Error check
-            if (sb.length() >= 2) {
-                if (String.valueOf(sb).equals(String.valueOf(sb2.reverse()))) {
-                    System.out.println("'" + sb + "'" + " IS A PALINDROME!");
+            // Error checking
+            if (words.length() >= 2) {
+                if (String.valueOf(words).equals(String.valueOf(reversedStringSplits.reverse()))) {
+                    System.out.println("'" + words + "'" + " IS A PALINDROME!");
                 } else {
-                    System.out.println("Nah Fam, " + "'" + sb + "'" + " is a regular word");
+                    System.out.println("Nah Fam, " + "'" + words + "'" + " is a regular word");
                 }
             } else {
                 System.out.println("Nah fam, you need at least 2 letters for palindrome");
             }
+
 
             System.out.println(" ");
         }
