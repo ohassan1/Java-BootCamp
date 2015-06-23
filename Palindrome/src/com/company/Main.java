f6d3bc17daaf820491aaf45998ab264c47f3b9c5
ohassan1 <nife.hassan@gmail.com>
2015-06-22 19:39:56 -0500

    Updated Palindrome to work without replace method

--- Palindrome/src/com/company/Main.java
+++ Palindrome/src/com/company/Main.java
@@ -3,12 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        StringBuilder palindrome = new StringBuilder("racecar");
+        String palindrome =  "racecar";
+
+        char[] testArray = new char[palindrome.length()];
 
 
-        if (palindrome == palindrome.reverse())
+        for (int i = palindrome.length(); i > 0; i--) {
+            testArray[i-1] = palindrome.charAt(palindrome.length() - (i));
+        }
+
+        String reverse = new String(testArray);
+
+        if (palindrome.equals(reverse))
         {
             System.out.println("This is a palindrome!!!!");
+        } else {
+            System.out.println("This is not a palindrome :((((");
         }
 
 
